13364df357496202c11c7e7701070c18766573d6
Yuni <20063249+yuniniwu@users.noreply.github.com>
2021-05-19 17:20:34 +0800

    init component: QueryOrder, AppendOrder

---
+++ src/components/AppendOrder/AppendOrder.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  /* padding-top: 64px; */
+  font-size: 1rem;
+`;
+
+export default function AppendOrder() {
+  return (
+    <>
+      <Container>AppendOrder</Container>
+    </>
+  );
+}
